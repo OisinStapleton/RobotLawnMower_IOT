4d990a8559477656abd321263313d8799b9c3944
OisinStapleton <G00365445@atu.ie>
2025-12-17 09:12:54 +0000

    Refactor motor control to use ledcWrite function
    Updated motor control code to use ledcWrite for PWM.

--- MotorDriverSampleCode.ino
+++ MotorDriverSampleCode.ino
@@ -1,23 +1,42 @@
-int E1 = 6;
-int M1 = 7;
-int E2 = 5;
-int M2 = 4;
+// Code to test Motors by stepping through from low to high speed, then high to low speed.
+
+#define E1 14
+#define M1 27
+#define E2 32
+#define M2 33
+
+#define PWM_FREQ 5000
+#define PWM_RES 8
 
 void setup()
 {
-    pinMode(M1, OUTPUT);
-    pinMode(M2, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+
+  ledcAttach(E1, PWM_FREQ, PWM_RES);
+  ledcAttach(E2, PWM_FREQ, PWM_RES);
+
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
 }
 
 void loop()
 {
-  int value;
-  for(value = 0 ; value <= 255; value+=5)
+  for(int value = 0; value <= 255; value += 5)
   {
-    digitalWrite(M1,HIGH);
-    digitalWrite(M2,HIGH);
-    analogWrite(E1, value);   //PWM Speed Control
-    analogWrite(E2, value);   //PWM Speed Control
+    ledcWrite(E1, value);
+    ledcWrite(E2, value);
     delay(30);
   }
+
+  delay(1000);
+
+  for(int value = 255; value >= 0; value -= 5)
+  {
+    ledcWrite(E1, value);
+    ledcWrite(E2, value);
+    delay(30);
+  }
+
+  delay(1000);
 }
