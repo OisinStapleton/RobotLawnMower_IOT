dc18c0f7204d9366dec8dd403b36fdebf4df54dd
OisinStapleton <G00365445@atu.ie>
2026-02-23 16:08:06 +0000

    Simple tutorial code to test motors

--- MotorDriverSampleCode.ino
+++ MotorDriverSampleCode.ino
@@ -1,42 +1,25 @@
 // Code to test Motors by stepping through from low to high speed, then high to low speed.
 
-#define E1 14
-#define M1 27
-#define E2 32
-#define M2 33
-
-#define PWM_FREQ 5000
-#define PWM_RES 8
+int E1 = 14;
+int M1 = 27;
+int E2 = 32;
+int M2 = 33;
 
 void setup()
 {
-  pinMode(M1, OUTPUT);
-  pinMode(M2, OUTPUT);
-
-  ledcAttach(E1, PWM_FREQ, PWM_RES);
-  ledcAttach(E2, PWM_FREQ, PWM_RES);
-
-  digitalWrite(M1, HIGH);
-  digitalWrite(M2, HIGH);
+    pinMode(M1, OUTPUT);
+    pinMode(M2, OUTPUT);
 }
 
 void loop()
 {
-  for(int value = 0; value <= 255; value += 5)
+  int value;
+  for(value = 0 ; value <= 255; value+=5)
   {
-    ledcWrite(E1, value);
-    ledcWrite(E2, value);
+    digitalWrite(M1,HIGH);
+    digitalWrite(M2,HIGH);
+    analogWrite(E1, value);   //PWM Speed Control
+    analogWrite(E2, value);   //PWM Speed Control
     delay(30);
   }
-
-  delay(1000);
-
-  for(int value = 255; value >= 0; value -= 5)
-  {
-    ledcWrite(E1, value);
-    ledcWrite(E2, value);
-    delay(30);
-  }
-
-  delay(1000);
 }
