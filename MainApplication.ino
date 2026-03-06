0b9716bba8ef79875f07da8155967ed3c3648bd5
OisinStapleton <G00365445@atu.ie>
2026-03-06 12:30:43 +0000

    Fixed bug(wrong #include format).

--- MainApplication.ino
+++ MainApplication.ino
@@ -1,41 +1,13 @@
-#include <Ultrasonic.h> 
-#include <MotorFunction.h> // Including all individual code from Arduino tabs.
-#include <Accelerometer.h>
+#include "Ultrasonic.h"
+#include "MotorFunction.h"
+#include "Accelerometer.h"
 
-#include <Wire.h>
-#include <Adafruit_MMA8451.h> // #includes necessary to run accelerometer
-#include <Adafruit_Sensor.h>
-
-Adafruit_MMA8451 mma = Adafruit_MMA8451();  
-
-const int TRIG_PIN = 12; //Trigger
-const int ECHO_PIN =14; //Echo
-long duration; //used to measure time from transmission while echo returns
-float cms, inches;
-
-int E1 = 14;
-int M1 = 27;
-int E2 = 32;
-int M2 = 33;
-
-void setup(){
-    // Serial port begin
+void setup() {
   Serial.begin(115200);
-
-  //define input and outputs
-  //Ultrasonic config
-  pinMode(TRIG_PIN,OUTPUT);
-  pinMode(ECHO_PIN,INPUT);
-
-// Motor config
-  pinMode(M1, OUTPUT);
-  pinMode(M2, OUTPUT);
-
-// Accelerometer config
-  Wire.begin(21, 22);  // SDA=21, SCL=22
+  AccelerometerSetup();
 }
 
-void loop(){
+void loop() {
   Ultrasonic();
   MotorFunction();
   Accelerometer();
