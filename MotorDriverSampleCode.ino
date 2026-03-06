5e0beaad070c2c58e609c99847b1ca8ef28a0825
OisinStapleton <G00365445@atu.ie>
2026-03-06 12:32:30 +0000

    Debug & switched E1 to pin26 to avoid Ultrasonic sensor overlap on pins

--- MotorDriverSampleCode.ino
+++ MotorDriverSampleCode.ino
@@ -1,25 +1,19 @@
-// Code to test Motors by stepping through from low to high speed, then high to low speed.
-
-int E1 = 14;
+int E1 = 26;
 int M1 = 27;
 int E2 = 32;
 int M2 = 33;
 
-void setup()
-{
-    pinMode(M1, OUTPUT);
-    pinMode(M2, OUTPUT);
-}
+void MotorFunction(){
 
-void loop()
-{
-  int value;
-  for(value = 0 ; value <= 255; value+=5)
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+
+  for(int value = 0 ; value <= 255; value+=5)
   {
     digitalWrite(M1,HIGH);
     digitalWrite(M2,HIGH);
-    analogWrite(E1, value);   //PWM Speed Control
-    analogWrite(E2, value);   //PWM Speed Control
+    analogWrite(E1, value);
+    analogWrite(E2, value);
     delay(30);
   }
 }
