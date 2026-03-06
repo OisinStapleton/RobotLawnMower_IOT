2d65108289f3d0193c0b0681f963a166de0930c1
OisinStapleton <G00365445@atu.ie>
2026-03-06 12:36:53 +0000

    Debug & created a seperate function for accelerometer setup.
    Refactored setup and loop functions into AccelerometerSetup and Accelerometer functions.

--- Accelerometer_Code.ino
+++ Accelerometer_Code.ino
@@ -1,28 +1,29 @@
-// Code to return X,Y and Z co-ordinates of an accelerometer.
 #include <Wire.h>
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
 
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin(21, 22);  // SDA=21, SCL=22
+void AccelerometerSetup(){
+
+  Wire.begin(21,22);
 
   if (!mma.begin()) {
     Serial.println("MMA8451 not found!");
-    while (1);
+    while(1);
   }
+
   Serial.println("MMA8451 found!");
 }
 
-void loop() {
+void Accelerometer(){
+
   sensors_event_t event;
   mma.getEvent(&event);
 
   Serial.print("X: "); Serial.print(event.acceleration.x);
-  Serial.print("  Y: "); Serial.print(event.acceleration.y);
-  Serial.print("  Z: "); Serial.println(event.acceleration.z);
+  Serial.print(" Y: "); Serial.print(event.acceleration.y);
+  Serial.print(" Z: "); Serial.println(event.acceleration.z);
 
   delay(1000);
 }
